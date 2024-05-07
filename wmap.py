67646956e2fe05c21f5cb8cfabf3a47e622e08fb
Wolfur1 <79707790+Wolfur1@users.noreply.github.com>
2024-05-07 16:04:29 +0200

    isOutOfMap fixed wmap.py
    Just making it better looking :)

--- wmap.py
+++ wmap.py
@@ -14,10 +14,8 @@ class wmap():
         self.replacement = 0
 
     def isOutOfMap(self, y = 0, x = 0):
-        if y > self.size_y-1 or x > self.size_x-1 or y<0 or x<0:
-            return True
-        else:
-            return False
+        if y > self.size_y-1 or x > self.size_x-1 or y<0 or x<0: return True
+        else: return False
 
     #Find the position of the cursor
     def findCursorPos(self, cursor) -> list:
